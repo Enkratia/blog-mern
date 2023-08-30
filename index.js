dd81001d5dd42e869270b010307a30930985864e
Enkratia <manofthetreasure@gmail.com>
2023-08-30 13:20:31 +0300

    m

--- index.js
+++ index.js
@@ -1,9 +1,29 @@
 import express from "express";
+import jwt from "jsonwebtoken";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/", (req, res) => {
   res.send("Hello world");
+});
+
+app.post("/auth/login", (req, res) => {
+  console.log(req.body);
+
+  const token = jwt.sign(
+    {
+      email: req.body.email,
+      fullName: "Вася Пупкин",
+    },
+    "testhash",
+  );
+
+  res.json({
+    success: true,
+    token,
+  });
 });
 
 app.listen(4444, (err) => {
