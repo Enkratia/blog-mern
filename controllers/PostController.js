1e81ad24c96c646eefc1c69e906b518f1babaf28
Enkratia <manofthetreasure@gmail.com>
2023-08-30 23:32:20 +0300

    m

---
+++ controllers/PostController.js
@@ -0,0 +1,137 @@
+import PostModel from "../models/Post.js";
+
+export const getAll = async (req, res) => {
+  try {
+    const posts = await PostModel.find().populate("user").exec();
+
+    res.json(posts);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      message: "Нет удалось получить статьи",
+    });
+  }
+};
+
+export const getOne = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    PostModel.findOneAndUpdate(
+      {
+        _id: postId,
+      },
+      {
+        $inc: {
+          viewsCount: 1,
+        },
+      },
+      {
+        returnDocument: "after",
+      },
+    )
+      .then((doc) => {
+        if (!doc) {
+          return res.status("404").json({
+            message: "Статья не найдена",
+          });
+        }
+
+        return res.json(doc);
+      })
+      .catch((err) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({
+            message: "Нет удалось вернуть статью",
+          });
+        }
+      });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      message: "Нет удалось получить статью",
+    });
+  }
+};
+
+export const remove = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    PostModel.findOneAndDelete({
+      _id: postId,
+    })
+      .then((doc) => {
+        if (!doc) {
+          return res.status(500).json({
+            message: "Статья не найдена",
+          });
+        }
+
+        res.json({
+          success: true,
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        return res.status(500).json({
+          message: "Нет удалось удалить статью",
+        });
+      });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      message: "Нет удалось удалить статью",
+    });
+  }
+};
+
+export const create = async (req, res) => {
+  try {
+    const doc = new PostModel({
+      title: req.body.title,
+      text: req.body.text,
+      imageUrl: req.body.imageUrl,
+      tags: req.body.tags,
+      user: req.userId, // вернуться, откуда берется id в рек тут?
+    });
+
+    const post = await doc.save();
+
+    res.json(post);
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      message: "Нет удалось создать статью",
+    });
+  }
+};
+
+export const update = async (req, res) => {
+  try {
+    const postId = req.params.id;
+
+    await PostModel.updateOne(
+      {
+        _id: postId,
+      },
+      {
+        title: req.body.title,
+        text: req.body.text,
+        imageUrl: req.body.imageUrl,
+        user: req.userId,
+        tags: req.body.tags,
+      },
+    );
+
+    res.json({
+      success: true,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(500).json({
+      message: "Нет удалось обновить статью",
+    });
+  }
+};
--- index.js
+++ index.js
@@ -1,30 +1,63 @@
 import express from "express";
-import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
+import multer from "multer";
+
+import { registerValidation, loginValidation, postCreateValidation } from "./validations.js";
+
+import { UserController, PostController } from "./controllers/index.js";
+
+import { handleValidationErrors, checkAuth } from "./utils/index.js";
+
+mongoose
+  .connect(
+    "mongodb+srv://manofthetreasure:wwwwww@cluster0.d877gnw.mongodb.net/blog?retryWrites=true&w=majority",
+  )
+  .then(() => console.log("DB ok"))
+  .catch((err) => console.log("DB error:" + err));
 
 const app = express();
 
-app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("Hello world");
+const storage = multer.diskStorage({
+  destination: (_, __, cb) => {
+    cb(null, "uploads");
+  },
+  filename: (_, file, cb) => {
+    cb(null, file.originalname);
+  },
 });
 
-app.post("/auth/login", (req, res) => {
-  console.log(req.body);
+const upload = multer({ storage });
 
-  const token = jwt.sign(
-    {
-      email: req.body.email,
-      fullName: "Вася Пупкин",
-    },
-    "testhash",
-  );
+app.use(express.json());
+app.use("/uploads", express.static("uploads"));
 
+app.post("/auth/login", loginValidation, handleValidationErrors, UserController.register);
+app.post("/auth/register", registerValidation, handleValidationErrors, UserController.login);
+app.get("/auth/me", checkAuth, UserController.getMe);
+
+app.post("/upload", checkAuth, upload.single("image"), (req, res) => {
   res.json({
-    success: true,
-    token,
+    url: `uploads/${req.file.originalname}`,
   });
 });
+
+app.get("/posts", PostController.getAll);
+app.get("/posts/:id", PostController.getOne);
+app.post(
+  "/posts/:id",
+  checkAuth,
+  postCreateValidation,
+  handleValidationErrors,
+  PostController.create,
+);
+app.delete("/posts/:id", checkAuth, PostController.remove);
+app.patch(
+  "/posts/:id",
+  checkAuth,
+  postCreateValidation,
+  handleValidationErrors,
+  PostController.update,
+);
 
 app.listen(4444, (err) => {
   if (err) {
